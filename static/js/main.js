d6a07e780c5a38e2cf2a097ddc0c9cc7d08372fb
Daniel Johansson <danjo133@gmail.com>
2015-08-30 22:36:16 +0200

    Add comments and log to html-list

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,59 @@
+// Just write a little debug data from the action
+function sendDataSuccess(elem) {
+    return function(data) {
+	console.log(elem + " successfully sent some data and got this response: " + data);
+    }
+}
+function sendDataFailure(elem) {
+    return function(data) {
+	console.log(elem + " failed to send some data and got this response: " + data);
+    }
+}
 
-function sendDataSuccess(data) {
-    console.log(data);
+function getDataSuccess(elem) {
+    return function(data) {
+	var json = JSON.parse(data);
+	console.log(json);
+	$.each(json["data"], function (item) {
+	    $(elem).append("<li>" + json["data"][item] + "</li>");
+	});
+    }
 }
 
 $(document).ready(function(){
     console.log("test logging");
 
-    $('button').click(function() {
-	data = {data: $(this).attr("data")};
+    // Handle all button control switches on the page
+    // They will make a post-request to <url>/send/<data-sensor-id>
+    $('.controlswitch').click(function() {
+	var id = $(this).attr("data-sensor-id");
+	var data = {data: $(this).attr("data-sensor-value")};
+	console.log(id);
+	console.log(data);
+
 	$.ajax({
-	    url: 'send/' + this.id,
+	    url: 'send/' + id,
 	    type: 'POST',
-	    data: JSON.stringify(data,null,'\t'), // An object with the key 'submit' and value 'true;
-	    success: sendDataSuccess,
+	    data: JSON.stringify(data, null, '\t'),
+	    success: sendDataSuccess(this),
+	    failure: sendDataFailure(this),
 	    contentType: "application/json; charset=utf-8"
 	});
     });
 
+    // Send a request for the available sensor log for the sensor id provided
+    $('.sensorlog').each(function(){
+	var id = $(this).attr("data-sensor-id");
+
+	if (id) {
+	    $.ajax({
+		url: 'get/' + id,
+		type: 'GET',
+		success: getDataSuccess(this),
+		contentType: "application/json; charset=utf-8"
+	    });
+	} else {
+	    console.log("Error, no id specified");
+	}
+    });
 });
